4ed1e761ae70eda4b66fcccbd850db6361706556
David Giese <johndgiese@gmail.com>
2012-06-15 00:25:38 -0400

    reshuffled windows stuff

--- onpath/cdh.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-cd %USERPROFILE%\Desktop
--- onpath/updatevimrc.bat
+++
@@ -1,7 +0,0 @@
-cd C:\opt\vim\vimfiles
-if not exist C:\opt\vim\vimfiles mkdir C:\opt\vim\vimfiles
-if not exist C:\opt\vim\tmp\undo mkdir C:\opt\vim\tmp\undo
-if not exist C:\opt\vim\tmp\backup mkdir C:\opt\vim\tmp\backup
-if not exist C:\opt\vim\tmp\swap mkdir C:\opt\vim\tmp\swap
-git pull
-if not exist C:\opt\vim\_vimrc mklink C:\opt\vim\_vimrc C:\opt\vim\vimfiles\_vimrc

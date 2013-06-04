f2bb6139e68c83d7c169ad67f75ec5fb085b8c7d
johndgiese <johndgiese@gmail.com>
2013-06-04 08:49:14 -0400

    add install procedure for windows

--- install.sh
+++ install.sh
@@ -1,11 +1,29 @@
 #!/bin/bash
 
+echo "LOOKING FOR VIM CUSTOMIZATION FOLDER..."
+if [ -d ".vim" ]; then
+    $DOTVIM = ".vim"
+    $OS = "NOT WINDOWS"
+elif [ -d "vimfiles" ]; then
+    $DOTVIM = "vimfiles"
+    $OS = "WINDOWS"
+else
+    echo "\nNo '.vim' or 'vimfiles' directories found!"
+    exit 1
+fi
+echo "\nFound vim customization folder: $DOTVIM."
+
 echo "SETTING UP SYMBOLIC LINKS..."
-ln -sv ~/.vim/vimrc ~/.vimrc
-ln -sv ~/.vim/gvimrc ~/.gvimrc
+if [ $OS == "WINDOWS"]; then
+    ln -sv ~/$DOTVIM/vimrc ~/_vimrc
+    ln -sv ~/$DOTVIM/gvimrc ~/_gvimrc
+else
+    ln -sv ~/$DOTVIM/vimrc ~/.vimrc
+    ln -sv ~/$DOTVIM/gvimrc ~/.gvimrc
+fi
 
 echo "\nINSTALLING VUNDLE, THE VIM PLUGIN MANAGER, ..."
-git clone -v https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+git clone -v https://github.com/gmarik/vundle.git ~/$DOTVIM/bundle/vundle
 
 echo "\nINSTALLING PLUGINS, MAY TAKE A WHILE ..."
 sleep 1

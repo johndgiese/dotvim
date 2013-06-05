9893d02dc7535e37b2d9e417d61d495b4dda2ae4
John David Giese <johndgiese@gmail.com>
2013-06-05 11:28:46 -0300

    Update install.bat

--- install.bat
+++ install.bat
@@ -2,12 +2,12 @@
 
 echo.
 echo "SETTING UP SYMBOLIC LINKS..."
-mklink %HOME%/_vimrc %HOME%/vimfiles/vimrc
-mklink %HOME%/_gvimrc %HOME%/vimfiles/gvimrc
+mklink %HOME%\_vimrc %HOME%\vimfiles\vimrc
+mklink %HOME%\_gvimrc %HOME%\vimfiles\gvimrc
 
 echo.
 echo "INSTALLING VUNDLE, THE VIM PLUGIN MANAGER, ..."
-git clone -v https://github.com/gmarik/vundle.git %HOME%/vimfiles/bundle/vundle
+git clone -v https://github.com/gmarik/vundle.git %HOME%\vimfiles\bundle\vundle
 
 echo.
 echo "INSTALLING PLUGINS, MAY TAKE A WHILE ..."

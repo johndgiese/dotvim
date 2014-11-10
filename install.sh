71423e90170c0eaa04b9ea275fb27c6217bfc766
J. David Giese <johndgiese@gmail.com>
2014-11-10 14:57:40 -0500

    Force link creation; don't install vundle if already exists

--- install.sh
+++ install.sh
@@ -2,17 +2,20 @@
 set -e
 
 echo -e "\nSETTING UP SYMBOLIC LINKS..."
-ln -sv $HOME/.vim/vimrc $HOME/.vimrc
-ln -sv $HOME/.vim/gvimrc $HOME/.gvimrc
+ln -svf $HOME/.vim/vimrc $HOME/.vimrc
+ln -svf $HOME/.vim/gvimrc $HOME/.gvimrc
 
-echo -e "\nINSTALLING VUNDLE, THE VIM PLUGIN MANAGER, ..."
-git clone -v https://github.com/gmarik/vundle.git ~/.vim/bundle/Vundle.vim
+if [[ -f $HOME/.vim/bundle/Vundle.vim ]]; then
+    echo -e "\nINSTALLING VUNDLE, THE VIM PLUGIN MANAGER, ..."
+    git clone -v https://github.com/gmarik/vundle.git ~/.vim/bundle/Vundle.vim
+else
+    echo -e "\nVUNDLE ALREADY INSTALLED"
+fi
 
 echo -e "\nINSTALLING PLUGINS, MAY TAKE A WHILE ..."
 vim +PluginInstall +qall
 
 echo -e "\nCompiling YouCompleteMe"
-cd $HOME/.vim/bundle/YouCompleteMe
-sh install.sh
+$HOME/.vim/bundle/YouCompleteMe/install.sh
 
 echo -e "\nFINISHED!  HAPPY VIMMING!"
